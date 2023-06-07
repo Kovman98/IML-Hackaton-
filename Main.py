dd9f89ddc3dca7f6339e7e496feea28967f5fd07
Anton Kravtsov <anton.kravtsov@mail.huji.ac.il>
2023-06-07 22:50:44 +0300

    preprocess updates in the Main file - Anton

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 import numpy as np
 import pandas as pd
+import agoda_preprocess
 import sklearn
 from sklearn import *
 from sklearn.linear_model import LinearRegression
@@ -35,14 +36,7 @@ def split_train_test(X: pd.DataFrame, y: pd.Series, train_proportion: float = .7
 
     test_y : Series of shape (floor((1-train_proportion) * n_samples), )
         Responses of test samples
-
     """
-
-    # train_x = X.sample(frac=train_proportion)
-    # train_y = pd.Series(y).loc[train_x.index]
-    # test_x = X.loc[X.index.difference(train_x.index)]
-    # test_y = pd.Series(y).loc[test_x.index]
-
 
     train_x: pd.DataFrame = X.sample(frac=train_proportion)
     train_y: pd.Series = y.loc[train_x.index]
@@ -53,15 +47,39 @@ def split_train_test(X: pd.DataFrame, y: pd.Series, train_proportion: float = .7
 
 
 if __name__ == '__main__':
-    # loading the data and dividing into X data frame and y vector
-    dataFrame: pd.DataFrame = pd.read_csv("agoda_cancellation_train.csv")
-    y = dataFrame["cancellation_datetime"]
+    # loading the data and shuffle it
+    dataFrame: pd.DataFrame = pd.read_csv("dataset/agoda_cancellation_train.csv")
+    shuffled_dataFrame = dataFrame.sample(frac=1).reset_index(drop=True)
+
+    # splitting into X and y
+    y: pd.Series = dataFrame["cancellation_datetime"]
     y = pd.Series(np.where(y == 0, 0, 1))
-    dataFrame = dataFrame.drop('cancellation_datetime', axis=1)
+    X: pd.DataFrame = dataFrame.drop('cancellation_datetime', axis=1)
 
     train_x, train_y, test_x, test_y = split_train_test(dataFrame, y)
 
-    fitted = LinearRegression().fit(train_x, train_y)
+
+    # corolated = X["customer_nationality"]
+    # label = X[""]
+    # for label in X:
+    #     rho = np.cov(X[f], y)[0, 1] / (np.std(X[f]) * np.std(y))
+
+    fitted: LinearRegression = LinearRegression().fit(train_x, train_y)
 
     # mse = metrics.mean_squared_error(y, fitted.predict(test_x))
     # print("Mean Squared Error:", mse)
+
+    df = pd.get_dummies(df, prefix='hotel_country_code_', columns=['hotel_country_code'])
+    df = pd.get_dummies(df, prefix='accommadation_type_name_', columns=['accommadation_type_name'])
+    df = pd.get_dummies(df, prefix='guest_nationality_country_name_', columns=['guest_nationality_country_name'])
+    df = pd.get_dummies(df, prefix='original_payment_type_', columns=['original_payment_type'])
+    df = pd.get_dummies(df, prefix='cancellation_policy_code_', columns=['cancellation_policy_code'])
+    df = pd.get_dummies(df, prefix='hotel_city_code_', columns=['hotel_city_code'])
+
+    X = X[X["hotel_star_rating"].between(0,5)]
+    X = X[X["no_of_adults"].isin(range(20))]
+    X = X[X["no_of_children"].isin(range(20))]
+    X = X[X["no_of_extra_bed"].isin(range(3))]
+    X = X[X["no_of_room"].isin(range(10))]
+
+
